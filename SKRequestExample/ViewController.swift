268df29fa06cfc379a2527d89486730b19364564
saber <saber@sabers-imac-484.local>
2016-08-14 09:46:26 +0430

    Initial Commit

---
+++ SKRequestExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SKRequestExample
+//
+//  Created by saber on 8/14/16.
+//  Copyright © 2016 saberk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
